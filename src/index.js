e3e02c1909ac72e01e7ca31972a1bc6d6a10192f
unworthyslaveTOCHRIST <righteousnessbyfaithinjesus@gmail.com>
2025-01-03 02:41:05 +0100

    Effective commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// ALL THANKS AND GLORY TO THE AND my ONLY GOD AND LORD JESUS CHRIST ALONE
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import App from "./project4/App"
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <App GTLJC_Christmode = {false}/>
+  </React.StrictMode>
+)
